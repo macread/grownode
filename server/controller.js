adb920aefc31e0f0a483b39b8f738b44b7de5c33
James 'Mac' Read <james.macgregor.read@gmail.com>
2018-12-14 15:57:36 -0700

    installed request

--- server/controller.js
+++ server/controller.js
@@ -1,6 +1,11 @@
 module.exports = {
     
     getPeople: (req, res, next) => {
+        const connection = req.app.get('db');
+        connection.contact_get([req.params.id])
+            .then ( (contacts) => {
+                res.status(200).send(contacts)} )
+            .catch ( (err) => res.status(500).send())
     },
 
     getPlanets: (req, res, next) => {
